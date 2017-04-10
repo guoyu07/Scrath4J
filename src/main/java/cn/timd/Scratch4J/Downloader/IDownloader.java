dafb7d104e4be58ca51d54afde01c80cfd45b8ca
周井江-技术中心-服务研发部 <zhoujingjiang@gomeplus.com>
2017-04-10 18:00:51 +0800

    refactor

--- src/main/java/cn/timd/Scratch4J/Downloader/IDownloader.java
+++
@@ -1,8 +0,0 @@
-package cn.timd.Scratch4J.Downloader;
-
-import cn.timd.Scratch4J.Request.IRequest;
-import cn.timd.Scratch4J.Response.IResponse;
-
-public interface IDownloader {
-    IResponse download(IRequest request);
-}
--- src/main/java/cn/timd/Scratch4J/Parser/IParser.java
+++
@@ -1,9 +0,0 @@
-package cn.timd.Scratch4J.Parser;
-
-import cn.timd.Scratch4J.Item.IItem;
-import cn.timd.Scratch4J.Request.IRequest;
-import cn.timd.Scratch4J.Response.IResponse;
-
-public interface IParser {
-    IItem parse(IRequest request, IResponse response);
-}
